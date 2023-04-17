288a48bcb789b030a3eba64de0ce91bad0c6ddc0
MartinaValkova <Martinavalkova16@hotmail.com>
2023-04-17 10:22:03 +0100

    Admin site and update of checkout and base.html

--- Eshop/shop/admin.py
+++ Eshop/shop/admin.py
@@ -4,5 +4,16 @@ from .models import Product, Order
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Order)
+admin.site.site_header = "E-Scooters & E-Bikes Shopping"
+admin.site.index_title = "Manage E-Scooters & E-Bikes Shopping Site"
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'discount_price', 'category', 'description', )
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('items', 'name', 'email', 'address', 'city', 'zipcode', 'region', 'total' )
+
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Order, OrderAdmin)
